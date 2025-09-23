4c880c37cdbfee5c0b50d7dab136c0397d040506
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-09-23 16:16:14 +0530

    Add files via upload

---
+++ Additem().py
@@ -0,0 +1,18 @@
+def additem(item, L=[]):
+    L.append(item)
+    return L
+
+L1=[1,2,3,4]
+
+additem(5,L1)
+
+print(L1)
+print(additem(22))
+print(additem(33))
+print(additem(44))
+
+L1=[1,2,3,4]
+additem(5,L1)
+print(L1)
+
+print(additem(55))
