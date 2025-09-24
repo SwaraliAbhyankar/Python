2563eeb1561d42932b1f2f7b1a2bc4837b66a052
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-09-24 14:06:54 +0530

    Add files via upload

---
+++ AND Operator.py
@@ -0,0 +1,8 @@
+math = float(input('Enter math marks '))
+phy = float(input("Enter physics marks "))
+chem = float(input("Enter chemistry marks "))
+if math >= 45 and phy >= 45 and chem >= 45:
+    print("Passed")
+else:
+    print("Failed")
+    
