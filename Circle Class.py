b216afb1d3cf180c645812ac099f94356a69a55e
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-09-26 22:16:53 +0530

    Add files via upload

---
+++ Circle Class.py
@@ -0,0 +1,16 @@
+import math
+
+class Circle:
+    def __init__(self, r):
+        self.radius = r 
+
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+
+c = Circle(3)
+print('Area:', c.area())
+print('Perimeter:', c.perimeter())
