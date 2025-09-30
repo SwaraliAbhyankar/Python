605340e9d018898474097b4e36ad8de1e4df4747
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-09-30 16:49:16 +0530

    Add files via upload

---
+++ Country names.py
@@ -0,0 +1,10 @@
+country={}
+
+for i in range(4):
+    name=input('Enter a country name: ')
+    if name[0].upper() not in country:
+        country[name[0].upper()]=[name]
+    else:
+        country[name[0].upper()].append(name)
+
+print(country)
