e4bbfcdb39092ff37b16028a2e73d9596e2f1450
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-10-03 10:05:40 +0530

    Add files via upload

---
+++ Default Arguments.py
@@ -0,0 +1,8 @@
+def add(a,b=5,c=7):     # try different values for b and c
+    return a+b+c
+
+print(add(10,20,30))
+print(add(10,20))
+print(add(10))
+print(add(a=10,b=5,c=2))
+print(add(20, b=10, c=5))
