ee3e25146e454f7184db718e0dbdd40c92af1db5
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-10-16 16:52:16 +0530

    Add files via upload

---
+++ Method Resolution Order.py
@@ -0,0 +1,12 @@
+class Parent:
+    def show(self):
+        print('Parent show')
+
+class Child(Parent):
+    def show(self):
+        print('Child show')
+
+c = Child()
+c.show()
+
+print(Child.mro())
