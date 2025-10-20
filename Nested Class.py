52b56983e32de8d1ddb567e30dc156173bf4550e
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-10-20 22:08:56 +0530

    Add files via upload

---
+++ Nested Class.py
@@ -0,0 +1,17 @@
+class Outer:
+    def __init__(self):
+        self.in_obj = self.Inner()
+
+    def show(self):
+        self.in_obj.display()
+
+    class Inner:
+        def __init__(self):
+            self.idata = 'Inner Data'
+
+        def display(self):
+            print(self.idata)
+
+
+o = Outer()
+o.show()
