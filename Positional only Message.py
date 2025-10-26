c2f363f7534d0c875baa89cae2b1d089d9e7b472
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-10-26 21:13:09 +0530

    Add files via upload

---
+++ Positional only Message.py
@@ -0,0 +1,5 @@
+def message(name, profession,/):
+    print(f'My name is {name}. I am a/an {profession}')
+    print('My name is '+name+'. I am a/an '+profession)
+
+message('Swarali', 'Data Scientist')
