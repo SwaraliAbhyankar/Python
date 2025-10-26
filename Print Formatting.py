7660fec33ca7e5c6f51a60f746f9506a25ea7bc3
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-10-26 21:13:39 +0530

    Add files via upload

---
+++ Print Formatting.py
@@ -0,0 +1,10 @@
+rollno=10
+name='Swarali'
+avg=88.314587596
+
+print('Student name is %s, her roll number is %d and the overall average is %f'
+                  %(name, rollno, avg))
+
+print('Student average is %g' %avg)
+print('Roll number is %10d' %rollno)
+print('Roll number is %-10d and average is %2.3f' %(rollno, avg))
