2218fa45d065e52ce55351a3edae8bbeccec4b51
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-11-04 09:38:03 +0530

    Add files via upload

---
+++ Sum of Inputs.py
@@ -0,0 +1,10 @@
+num_of_nos = int(input("Enter number of numbers: "))
+Sum = 0
+count = 0
+
+while count < num_of_nos:
+    n = int(input("Enter number: "))
+    Sum += n
+    count += 1
+
+print("Sum of numbers is ", Sum)
