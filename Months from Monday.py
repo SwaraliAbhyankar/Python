82fe31e867180f9a91e674646841893f06149e6a
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-11-05 10:43:00 +0530

    Add files via upload

---
+++ Months from Monday.py
@@ -0,0 +1,11 @@
+import datetime
+year = int(input("Enter year: "))
+monday = 0
+
+for month in range(1,13): 
+    dt = datetime.date(year,month,1)
+    if dt.weekday() == 0: 
+        monday += 1
+        print(monday)
+
+print("No. of months starting from Monday: ", monday)
