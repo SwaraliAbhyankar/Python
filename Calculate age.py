ff772efb046b3db1631c609f70ebb726fc0ad0d7
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-11-05 10:43:31 +0530

    Add files via upload

---
+++ Calculate age.py
@@ -0,0 +1,11 @@
+from datetime import date
+def age(dob):
+    today = date.today()
+    years = today.year - dob.year
+
+    if (today.month, today.day) < (dob.month, dob.day): 
+        years -= 1
+
+    return years
+
+print('Age: ', age(date(2003,3,1)))
