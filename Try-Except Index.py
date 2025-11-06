836545fedb8960c8f5c3cd87a6689f30e32ab386
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-11-06 16:54:18 +0530

    Add files via upload

---
+++ Try-Except Index.py
@@ -0,0 +1,10 @@
+L=[10,20,30,40,50]
+
+try: 
+    index = 'bcd' #try using string and userinput
+    print(L[index])
+    print('End of try')
+except:
+    print('Invalid index!')
+
+print('End of program')
