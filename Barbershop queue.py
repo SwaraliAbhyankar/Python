cc508c435f8575000cfe1fccbe4b8c55c917917e
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-11-13 11:14:25 +0530

    Add files via upload

---
+++ Barbershop queue.py
@@ -0,0 +1,18 @@
+from collections import deque
+customers = deque()
+
+def walk_in(customer):
+    customers.append(customer)
+
+def served():
+    cust = customers.popleft()
+    print(cust, 'leaves the shop')
+
+walk_in('John')
+walk_in('James')
+walk_in('Smith')
+served()
+served()
+walk_in('Mark')
+
+print(customers)
