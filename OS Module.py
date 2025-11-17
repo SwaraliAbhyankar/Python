3315444df90a29bc76bca1e7b8cb703374ccd567
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-11-17 15:20:44 +0530

    Add files via upload

---
+++ OS Module.py
@@ -0,0 +1,23 @@
+import os
+
+# os.name
+# os.getlogin()
+# os.getcwd()
+
+# os.chdir('C:\OneDrive\Desktop')
+# os.getcwd()
+
+# os.listdir()
+# os.mkdir('C:\OneDrive\Desktop\ABC')
+# os.makedirs('C:\OneDrive\Desktop\GParent\Parent\Child')
+
+# os.remove('C:\OneDrive\Desktop\ABC\sunflower.jpg')
+# os.rmdir('C:\OneDrive\Desktop\ABC')
+# os.removedirs('C:\OneDrive\Desktop\GParent\Parent\Child')
+
+# os.rename('C:\OneDrive\Desktop\Mob App Images', 'C:\OneDrive\Desktop\Mob Img')
+
+
+# All these are commented to not make any changes in the existing system.
+# These commands are only for understanding and learnign purposes.
+# Do not execute them.
