a55e67953a4faca0db1d8c9182b86ae237bdf646
Swarali Abhyankar <abhyankarswarali133@gmail.com>
2025-11-20 19:03:38 +0530

    Add files via upload

---
+++ Array indexing.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+ar1 = np.array([1,2,3,4,5,6,7,8,9,10])
+ar2 = np.array([[2,4,6,8], [1,3,5,7], [10,20,30,40]])
+
+print(ar1[4])
+print(ar1[-3])
+print(ar2[2,3])
+print(ar2[-1,-3])
+print(ar2[-3,-4])
